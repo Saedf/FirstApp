3ca9558b509de3cb3c1770603295cfb0ba7071b8
saedf <saed.fathi@gmail.com>
2018-04-30 18:25:38 +0430

    End Session 15and16

--- app/src/main/java/com/example/saedf/app7learn/PostActivity.java
+++ app/src/main/java/com/example/saedf/app7learn/PostActivity.java
@@ -2,12 +2,32 @@ package com.example.saedf.app7learn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class PostActivity extends AppCompatActivity {
+import com.example.saedf.app7learn.Adapter.PostAdapter;
+import com.example.saedf.app7learn.ApiService.ApiService;
+import com.example.saedf.app7learn.dataModel.Post;
+
+import java.util.List;
+
+public class
+PostActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+        ApiService apiService=new ApiService(this);
+        apiService.getPosts(new ApiService.OnRecivedNews() {
+            @Override
+            public void onrecived(List<Post> postList) {
+                RecyclerView recyclerView=findViewById(R.id.rv_activitypost);
+                PostAdapter postAdapter=new PostAdapter(PostActivity.this,postList);
+                recyclerView.setLayoutManager(new LinearLayoutManager(PostActivity.this,
+                        LinearLayoutManager.VERTICAL,false));
+                recyclerView.setAdapter(postAdapter);
+            }
+        });
     }
 }
