39aa017c0c23db3d73fc93611552784ae2af71cc
saedf <saed.fathi@gmail.com>
2018-05-06 17:27:25 +0430

    start Sesion 23

--- app/src/main/java/com/example/saedf/app7learn/MainActivity.java
+++ app/src/main/java/com/example/saedf/app7learn/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.saedf.app7learn;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,6 +17,7 @@ import com.example.saedf.app7learn.dataFake.FeatureDataFakeGenerator;
 
 public class MainActivity extends AppCompatActivity {
 private AppFeatureAdapter appFeatureAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,16 +25,19 @@ private AppFeatureAdapter appFeatureAdapter;
         setupView();
     }
 
+
     private void setupView() {
         setupRecyclerView();
         setupToolbar();
 
     }
 
+
     private void setupToolbar() {
         DrawerLayout drawerLayout=findViewById(R.id.drawer_layout);
 
         Toolbar toolbar=findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(ContextCompat.getColor(this,R.color.is_not_visited));
         setSupportActionBar(toolbar);
         ActionBar actionBar=getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
